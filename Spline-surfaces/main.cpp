4e8b891eda4d56e5c6a568516ea3f2df4bac9e51
FanniBelics <fanni.belics@gmail.com>
2024-09-29 20:15:00 +0200

    Getting N and M from user input

--- Spline-surfaces/main.cpp
+++ Spline-surfaces/main.cpp
@@ -8,6 +8,8 @@
 
 using namespace std;
 
+int N =4 , M = 4;
+
 // Initialization routine.
 void setup(void)
 {
@@ -18,8 +20,61 @@ void setup(void)
 void drawScene(void)
 {
 
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+	glColor3f(0.518, 0.8, 0.82);
+	glBegin(GL_TRIANGLE_STRIP);
+
+	glVertex3f(30.0, 30.0, 0.0);
+	glVertex3f(20.0, 20.0, 0.0);
+	glVertex3f(30.0, 70.0, 0.0);
+	glVertex3f(20.0, 80.0, 0.0);
+	glVertex3f(70.0, 70.0, 0.0);
+	glVertex3f(80.0, 80.0, 0.0);
+	glVertex3f(70.0, 30.0, 0.0);
+	glVertex3f(80.0, 20.0, 0.0);
+	glVertex3f(30.0, 30.0, 0.0);
+	glVertex3f(20.0, 20.0, 0.0);
+
+	glEnd();
+
+	//Inlines
+	glColor3f(0.0, 0.0, 0.0);
+	glBegin(GL_LINE_STRIP);
+
+	glVertex3f(70.0, 30.0, 0.0);
+	glVertex3f(80.0, 20.0, 0.0);
+	glVertex3f(30.0, 30.0, 0.0);
+	glVertex3f(20.0, 20.0, 0.0);
+	glVertex3f(30.0, 70.0, 0.0);
+	glVertex3f(20.0, 80.0, 0.0);
+	glVertex3f(70.0, 70.0, 0.0);
+	glVertex3f(80.0, 80.0, 0.0);
+	glVertex3f(70.0, 30.0, 0.0);
+
+	glEnd();
+
+	//Inner outline
+	glColor3f(0.345, 0.557, 0.553);
+	glBegin(GL_LINE_LOOP);
+	glVertex3f(30.0, 30.0, 0.0);
+	glVertex3f(30.0, 70.0, 0.0);
+	glVertex3f(70.0, 70.0, 0.0);
+	glVertex3f(70.0, 30.0, 0.0);
+
+	glEnd();
+
+	//Outer Outline
+	glBegin(GL_LINE_LOOP);
+	glVertex3f(20.0, 20.0, 0.0);
+	glVertex3f(20.0, 80.0, 0.0);
+	glVertex3f(80.0, 80.0, 0.0);
+	glVertex3f(80.0, 20.0, 0.0);
+
+	glEnd();
 
 	glFlush();
 }
@@ -30,8 +85,15 @@ void resize(int w, int h)
 	glViewport(0, 0, w, h);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glFrustum(-5.0, 5.0, -5.0, 5.0, 5.0, 100.0);
+
+	// Use orthographic projection (appropriate for 2D drawing)
+	if (w <= h)
+		glOrtho(0.0, 100.0, 0.0, 100.0 * (GLfloat)h / (GLfloat)w, -1.0, 1.0);
+	else
+		glOrtho(0.0, 100.0 * (GLfloat)w / (GLfloat)h, 0.0, 100.0, -1.0, 1.0);
+
 	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 }
 
 // Keyboard input processing routine.
@@ -50,6 +112,13 @@ void keyInput(unsigned char key, int x, int y)
 // Routine to output interaction instructions to the C++ window.
 void printInteraction(void)
 {
+	cout << "Please provide the N and M as the size of the pane" << endl;
+	cout << "If N and M are not proper they are set 4 by default" << endl;
+	cout << "N: "; cin >> N;
+	if (N <= 1) N = 4;
+	cout << "M: "; cin >> M;
+	if (M <= 1) M = 4;
+	cout << "Set, N: " << N << " M: " << M << endl;
 }
 
 // Main routine.
